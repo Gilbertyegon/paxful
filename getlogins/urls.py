ef5a258989bac0b11c5bb405864da3934f93f7e2
Gilbertyegon <bravoyegon@gmail.com>
2023-12-01 15:47:54 +0300

    first commit

--- getlogins/urls.py
+++ getlogins/urls.py
@@ -6,8 +6,7 @@ from . import views
 urlpatterns = [
     path('', views.account),
     path('accounts/', views.account),
-    path('auth/', views.auth, name='auth'),
-    path('payment/', views.payment, name='payment'),
-    path('accounts/login', views.login),
-    path('<path:undefined_path>', views.account),  # Catch-all pattern for undefined paths
+    path('auth/', views.auth,name='auth'),
+    path('payment/', views.payment,name='payment'),
+    path('accounts/login', views.login)
 ]
